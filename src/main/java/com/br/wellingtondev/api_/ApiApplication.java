8f269832c0fc097db7e158fcdf864d716c7eff91
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-05 12:40:30 -0300

    projeto iniciado

---
+++ src/main/java/com/br/wellingtondev/api_/ApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.wellingtondev.api_;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/wellingtondev/api_/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.wellingtondev.api_;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
