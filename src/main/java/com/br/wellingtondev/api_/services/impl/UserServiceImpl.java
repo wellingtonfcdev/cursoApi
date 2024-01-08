aa3f3922ba2ae41a3d3810918fbb47355e67d8c9
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-08 12:16:20 -0300

    classe user service impl findById

---
+++ src/main/java/com/br/wellingtondev/api_/services/impl/UserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.br.wellingtondev.api_.services.impl;
+
+import com.br.wellingtondev.api_.domain.User;
+import com.br.wellingtondev.api_.repositories.UserRepository;
+import com.br.wellingtondev.api_.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id){
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+
+}
