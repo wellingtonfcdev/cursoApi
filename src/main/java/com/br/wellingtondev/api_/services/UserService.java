d9db4a04b395be52323166a9685af59f6d2cad4f
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-09 14:26:21 -0300

    EndPoint FindAll para user

--- src/main/java/com/br/wellingtondev/api_/services/UserService.java
+++ src/main/java/com/br/wellingtondev/api_/services/UserService.java
@@ -2,7 +2,10 @@ package com.br.wellingtondev.api_.services;
 
 import com.br.wellingtondev.api_.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
--- src/main/java/com/br/wellingtondev/api_/services/impl/UserServiceImpl.java
+++ src/main/java/com/br/wellingtondev/api_/services/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.br.wellingtondev.api_.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -18,5 +19,9 @@ public class UserServiceImpl implements UserService {
     public User findById(Integer id){
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado!!"));
+    }
+
+    public List<User> findAll(){
+        return repository.findAll();
     }
 }
