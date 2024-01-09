14cd5b188d465b1984c710b69429257d410a2e2f
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-09 01:56:49 -0300

    Classe User resource

--- src/main/java/com/br/wellingtondev/api_/resources/UserResource.java
+++ src/main/java/com/br/wellingtondev/api_/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.br.wellingtondev.api_.resources;
 
 import com.br.wellingtondev.api_.domain.User;
+import com.br.wellingtondev.api_.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
 
-        return ResponseEntity.ok().body(new User(1,"wellington","well@teste.com","123"));
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
