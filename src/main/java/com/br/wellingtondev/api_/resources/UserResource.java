ec888ad6e10d9e3f2016a491106d7dd7ab160685
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-09 01:43:39 -0300

    Classe User Resource findById

---
+++ src/main/java/com/br/wellingtondev/api_/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.br.wellingtondev.api_.resources;
+
+import com.br.wellingtondev.api_.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserResource {
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+
+        return ResponseEntity.ok().body(new User(1,"wellington","well@teste.com","123"));
+    }
+}
