75a9113a6c1cf3d03585777f2b54db2f8445cb71
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-09 01:18:04 -0300

    Perfil de teste

---
+++ src/main/java/com/br/wellingtondev/api_/config/LocalConfig.java
@@ -0,0 +1,27 @@
+package com.br.wellingtondev.api_.config;
+
+import com.br.wellingtondev.api_.domain.User;
+import com.br.wellingtondev.api_.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+
+public class LocalConfig {
+
+@Autowired
+private UserRepository repository;
+
+    @Bean
+    public void startDB() {
+        User u1 = new User(null, "Wellington", "wellington@teste.com", "123");
+        User u2 = new User(null, "Joaquim", "joaquim@teste.com", "123");
+
+        repository.saveAll(List.of(u1,u2));
+    }
+}
---
+++ src/main/java/com/br/wellingtondev/api_/domain/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package com.br.wellingtondev.api_.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+}
