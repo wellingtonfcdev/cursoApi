7c7dcffba005b78bd32089fba70ea78190bf9fe0
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-11 01:06:33 -0300

    EndPoint para delete do user

--- src/main/java/com/br/wellingtondev/api_/resources/UserResource.java
+++ src/main/java/com/br/wellingtondev/api_/resources/UserResource.java
@@ -18,11 +18,12 @@ import java.util.stream.Collectors;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    public static final String ID = "/{id}";
     @Autowired
     private ModelMapper mapper;
     @Autowired
     private UserService service;
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = ID)
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
 
         return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
@@ -39,16 +40,21 @@ public class UserResource {
     @PostMapping
     public ResponseEntity<UserDTO> create(@RequestBody UserDTO ojb){
 
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path(ID)
                 .buildAndExpand(service.create(ojb).getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = ID)
     public ResponseEntity<UserDTO> update(@PathVariable Integer id, @RequestBody UserDTO obj){
         obj.setId(id);
 
         return ResponseEntity.ok().body(mapper.map(service.update(obj), UserDTO.class));
     }
 
+    @DeleteMapping(value = ID)
+    public ResponseEntity<UserDTO> delete(@PathVariable Integer id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/br/wellingtondev/api_/services/impl/UserServiceImpl.java
+++ src/main/java/com/br/wellingtondev/api_/services/impl/UserServiceImpl.java
@@ -43,6 +43,13 @@ public class UserServiceImpl implements UserService {
         return repository.save(mapper.map(obj, User.class));
     }
 
+    @Override
+    public void delete(Integer id) {
+        findById(id);
+        repository.deleteById(id);
+    }
+
+
     private void findByEmail(UserDTO obj){
         Optional<User> user = repository.findByEmail(obj.getEmail());
 
