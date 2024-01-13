e597e08460c43b5f68526e7c73e9e9bd2fea6431
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-13 01:27:52 -0300

    Teste para classe user service metodo update com excecao

--- src/test/java/com/br/wellingtondev/api_/services/impl/UserServiceImplTest.java
+++ src/test/java/com/br/wellingtondev/api_/services/impl/UserServiceImplTest.java
@@ -131,6 +131,20 @@ class UserServiceImplTest {
     }
 
     @Test
+    void whenUpdateThenReturnAnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try{
+            optionalUser.get().setId(2);
+            service.create(userDTO);
+        }catch(Exception ex){
+            assertEquals(DataIntegratyViolationException.class, ex.getClass());
+            assertEquals("E-mail já cadastrado no sistema!",ex.getMessage());
+        }
+
+    }
+
+    @Test
     void delete() {
     }
 
