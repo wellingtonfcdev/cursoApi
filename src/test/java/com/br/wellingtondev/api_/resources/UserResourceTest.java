f71f365595fb036668f040f21b0df7da8bf756fe
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-13 02:05:45 -0300

    Teste para endpoint findById da classe user resource implementado

--- src/test/java/com/br/wellingtondev/api_/resources/UserResourceTest.java
+++ src/test/java/com/br/wellingtondev/api_/resources/UserResourceTest.java
@@ -3,17 +3,22 @@ package com.br.wellingtondev.api_.resources;
 import com.br.wellingtondev.api_.domain.User;
 import com.br.wellingtondev.api_.domain.dto.UserDTO;
 import com.br.wellingtondev.api_.services.impl.UserServiceImpl;
+import org.hibernate.EmptyInterceptor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserResourceTest {
@@ -40,7 +45,20 @@ class UserResourceTest {
     }
 
     @Test
-    void findById() {
+    void whenFindByIdReturnSuccess() {
+        when(service.findById(anyInt())).thenReturn(user);
+        when(mapper.map(any(), any())).thenReturn(userDTO);
+
+        ResponseEntity<UserDTO> response = resource.findById(ID);
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(UserDTO.class, response.getBody().getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getNome());
+        assertEquals(EMAIL, response.getBody().getEmail());
+        assertEquals(PASSWORD,response.getBody().getPassword());
     }
 
     @Test
