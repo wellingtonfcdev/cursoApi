323c8761a4d6d5700a8eb05d120878fc643af085
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-13 03:20:55 -0300

    Removendo anotacoes nao utilizadas

--- src/main/java/com/br/wellingtondev/api_/domain/dto/UserDTO.java
+++ src/main/java/com/br/wellingtondev/api_/domain/dto/UserDTO.java
@@ -1,6 +1,6 @@
 package com.br.wellingtondev.api_.domain.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
--- src/main/java/com/br/wellingtondev/api_/resources/exceptions/StandartError.java
+++ src/main/java/com/br/wellingtondev/api_/resources/exceptions/StandartError.java
@@ -2,12 +2,11 @@ package com.br.wellingtondev.api_.resources.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
+
 
 import java.time.LocalDateTime;
 
 @Getter
-@Setter
 @AllArgsConstructor
 public class StandartError {
 
