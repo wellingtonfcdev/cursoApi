568717f66054db69824de9fcdb46d07e49d82e1f
WellingtonDev <wellingtonfc95@gmail.com>
2024-01-13 01:33:11 -0300

    Teste para classe user service metodo delete com sucesso

--- src/test/java/com/br/wellingtondev/api_/services/impl/UserServiceImplTest.java
+++ src/test/java/com/br/wellingtondev/api_/services/impl/UserServiceImplTest.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 class UserServiceImplTest {
@@ -145,7 +145,11 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUser);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
     }
 
     private void startUser(){
